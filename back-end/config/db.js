830df549cdad8f2b16d06a706e9cdd9497ea4199
Koushik Pulavarthi <koushipulavarthi@gmail.com>
2026-04-06 14:39:11 +0530

    fixes mongo

--- back-end/config/db.js
+++ back-end/config/db.js
@@ -1,7 +1,8 @@
 import mongoose from 'mongoose';
 
-export const connectDB = async()=>
-{
-    await mongoose.connect('mongodb+srv://koushipulavarthi_db_user:DZX1PVV2Kihdj57j@cluster0.mjfwroe.mongodb.net/?appName=Cluster0/Expense')
-    .then(()=>console.log('DB Connected'))
+export const connectDB = async () => {
+    await mongoose.connect(
+        'mongodb+srv://koushipulavarthi_db_user:DZX1PVV2Kihdj57j@cluster0.mjfwroe.mongodb.net/Expense?retryWrites=true&w=majority'
+    )
+    .then(() => console.log('DB Connected'))
 }
