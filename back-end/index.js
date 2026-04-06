57e201694bc00ed2787ccb142eef93b6a14f35df
Koushik Pulavarthi <koushipulavarthi@gmail.com>
2026-04-06 15:49:38 +0530

    fixed cors

--- back-end/index.js
+++ back-end/index.js
@@ -17,7 +17,7 @@ app.use(cors({
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   credentials: true
 }));
-app.options('*', cors());
+
 app.use(express.json())
 app.use(express.urlencoded({extended:true}));
 
