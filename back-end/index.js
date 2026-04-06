8cf03e26ac4deb3e5fd0c3a68825bae4bf15e308
Koushik Pulavarthi <koushipulavarthi@gmail.com>
2026-04-06 15:47:43 +0530

    fix cors

--- back-end/index.js
+++ back-end/index.js
@@ -12,7 +12,12 @@ const app = express();
 const port = 4000;
 
 //Middlewares
-app.use(cors());
+app.use(cors({
+  origin: 'https://expense-tracker-koushik.netlify.app',
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  credentials: true
+}));
+app.options('*', cors());
 app.use(express.json())
 app.use(express.urlencoded({extended:true}));
 
