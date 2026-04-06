198e8b5034775de07f780b80afaa28d61092b893
Koushik Pulavarthi <koushipulavarthi@gmail.com>
2026-04-06 17:29:14 +0530

    edit

--- back-end/config/db.js
+++ back-end/config/db.js
@@ -1,7 +1,7 @@
 // import mongoose from 'mongoose';
 // import dotenv from 'dotenv';
 
-dotenv.config();
+// dotenv.config();
 
 export const connectDB = async () => {
     await mongoose.connect(process.env.MONGO_URI)
